f9c1872e7538397b578f7bdd015c68e1a84ba0b2
Elton Lobo <elton1231@gmail.com>
2021-11-23 18:04:18 +0400

    fix: is_owner check

--- frappe/permissions.py
+++ frappe/permissions.py
@@ -160,7 +160,7 @@ def get_role_permissions(doctype_meta, user=None, is_owner=None):
 
 	if not user: user = frappe.session.user
 
-	cache_key = (doctype_meta.name, user)
+	cache_key = (doctype_meta.name, user, cint(is_owner))
 
 	if user == 'Administrator':
 		return allow_everything()
